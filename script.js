d48aeb248c2cb22d79341c6ab92c45324156e974
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-12-26 15:17:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    displayDefaultMovies();
+});
+
+async function displayDefaultMovies() {
+    const response = await fetch(`https://www.dark-yasiya-api.site/movie/sinhalasub/search?text=popular`);
+    const data = await response.json();
+    
+    const movieContainer = document.getElementById('movieContainer');
+    movieContainer.innerHTML = '';
+    
+    if (data.status && data.result) {
+        const movies = data.result.data;
+        movies.forEach(movie => {
+            const movieElement = document.createElement('div');
+            movieElement.classList.add('movie');
+            
+            movieElement.innerHTML = `
+                <img src="${movie.image}" alt="${movie.title}">
+                <h2>${movie.title}</h2>
+                <p>${movie.imdb}</p>
+                <p>${movie.year}</p>
+                <a href="${movie.link}" target="_blank">Watch Now</a>
+            `;
+            
+            movieContainer.appendChild(movieElement);
+        });
+    } else {
+        movieContainer.innerHTML = '<p>No movies found</p>';
+    }
+}
+
 async function searchMovies() {
     const searchInput = document.getElementById('searchInput').value;
     const response = await fetch(`https://www.dark-yasiya-api.site/movie/sinhalasub/search?text=${encodeURIComponent(searchInput)}`);
